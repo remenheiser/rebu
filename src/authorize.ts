51711b654403ab96fa457afb3179a16773b183b6
Ryan Emenheiser <ryane@udel.edu>
2019-12-16 20:28:02 -0500

    fixed errors in authorize.ts

--- src/authorize.ts
+++ src/authorize.ts
@@ -3,16 +3,17 @@ const passportJWT = require("passport-jwt");
 const ExtractJWT = passportJWT.ExtractJwt;
 const LocalStrategy = require('passport-local').Strategy;
 const JWTStrategy   = passportJWT.Strategy;
+import { UserSchema } from "./models/user";
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
 },
 
-function (email, password, cb) {
+function (email: any, password: any, cb: any) {
 
     //Assume there is a DB module pproviding a global UserModel
-    return UserModel.findOne({email, password})
+    return UserSchema.findOne({email, password})
         .then(user => {
             if (!user) {
                 return cb(null, false, {message: 'Incorrect email or password.'});
@@ -32,10 +33,10 @@ passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey   : 'your_jwt_secret'
 },
-function (jwtPayload, cb) {
+function (jwtPayload: any, cb: any) {
 
     //find the user in db if needed
-    return UserModel.findOneById(jwtPayload.id)
+    return UserSchema.findOne(jwtPayload.id)
         .then(user => {
             return cb(null, user);
         })
